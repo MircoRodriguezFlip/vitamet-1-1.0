a874cdc5b0a3c5e2e7c89ba5a83953ed4ca48fc5
MircoRodriguez <githubflip@gmail.com>
2025-02-10 14:46:41 -0300

    20 vitamet commit - section 5 1.2

--- src/components/imports/Section5Lp.jsx
+++ src/components/imports/Section5Lp.jsx
@@ -16,31 +16,35 @@ export const Section5LP = () => {
             </div>
 
             <div className="section-5-2">
-                <div className="container-texto-general">
-                    <img src={fotoTexto1} alt="Alcancía de cochinito a la que le está entrando dinero" loading="lazy" decoding="async" />
+                <div className="section-5-2-1">
+                    <div className="container-texto-general">
+                        <img src={fotoTexto1} alt="Alcancía de cochinito a la que le está entrando dinero" loading="lazy" decoding="async" />
 
-                    <div className="container-texto-section-5">
-                        <p className="bold-text">Rendimiento garantizado del 2% nominal anual sobre la reserva de la prima de las coberturas.</p>
+                        <div className="container-texto-section-5">
+                            <p className="bold-text">Rendimiento garantizado del 2% nominal anual sobre la reserva de la prima de las coberturas.</p>
+                        </div>
+                    </div>
+
+                    <div className="container-texto-general">
+                        <img src={fotoTexto2} alt="Lapida con flores" loading="lazy" decoding="async" />
+
+                        <div className="container-texto-section-5">
+                            <p className="bold-text">En caso de fallecimiento, se incluye el servicio de asistencia funeraria.</p>
+                        </div>
+                    </div>
+
+                    <div className="container-texto-general">
+                        <img src={fotoTexto3} alt="Etiquetas rojas con un símbolo de porcentaje" loading="lazy" decoding="async" />
+
+                        <div className="container-texto-section-5">
+                            <p className="bold-text">
+                                Ofrece descuentos con proveedores, tarifas preferenciales, consultas médicas telefónicas, y más.
+                            </p>
+                        </div>
                     </div>
                 </div>
 
-                <div className="container-texto-general">
-                    <img src={fotoTexto2} alt="Lapida con flores" loading="lazy" decoding="async" />
-
-                    <div className="container-texto-section-5">
-                        <p className="bold-text">En caso de fallecimiento, se incluye el servicio de asistencia funeraria.</p>
-                    </div>
-                </div>
-
-                <div className="container-texto-general">
-                    <img src={fotoTexto3} alt="Etiquetas rojas con un símbolo de porcentaje" loading="lazy" decoding="async" />
-
-                    <div className="container-texto-section-5">
-                        <p className="bold-text">Ofrece descuentos con proveedores, tarifas preferenciales, consultas médicas telefónicas, y más.</p>
-                    </div>
-                </div>
-
-                <div className="imagen-section-5-1" ref={imageRef}>
+                <div className="imagen-section-5" ref={imageRef}>
                     <img
                         src={fondoImgSection}
                         className="img-1-section-5"
@@ -56,23 +60,6 @@ export const Section5LP = () => {
                         decoding="async"
                     />
                 </div>
-            </div>
-
-            <div className="imagen-section-5" ref={imageRef}>
-                <img
-                    src={fondoImgSection}
-                    className="img-1-section-5"
-                    alt="Luz que te invita a leer los beneficios"
-                    loading="lazy"
-                    decoding="async"
-                />
-                <img
-                    src={imgSection5}
-                    className="img-2-section-5"
-                    alt="Mujer joven muy feliz al ver los beneficios"
-                    loading="lazy"
-                    decoding="async"
-                />
             </div>
         </section>
     );
